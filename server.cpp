8d98bf296af8cbb636af3e5799b57c9e711a4dc9
Jonathan Lam <jlam7@g.ucla.edu>
2022-02-21 21:16:57 -0800

    First commit on my private server branch. Added initial include statements. Added ability to read inputs from CLI

--- server.cpp
+++ server.cpp
@@ -1,8 +1,32 @@
 #include <string>
 #include <thread>
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <time.h>
+#include <signal.h>
+#include <errno.h>
 
-int main()
+using namespace std;
+
+#define MAX_SIZE 524
+#define MAX_ACK 102400
+#define MIN_CWND 512
+#define MAX_CWND 51200
+#define RWND 51200
+#define Init_SSTHRESH 10000
+
+bool socket_setup(int port);
+
+int main(int argc, char *argv[])
 {
-  std::cerr << "server is not implemented yet" << std::endl;
+  int port = atoi(argv[1]);
+  socket_setup(port);
+}
+
+bool socket_setup(int port) {
+  cout << "port num: " << port << endl;
+  return true;
 }
