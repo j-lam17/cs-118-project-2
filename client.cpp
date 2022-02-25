cbe06adf2845a084144210ab12df81f536739fb4
Desmond Andersen <dfa@ucla.edu>
2022-02-24 22:11:10 -0800

    Set up basic connection

--- client.cpp
+++ client.cpp
@@ -1,8 +1,82 @@
-#include <string>
-#include <thread>
-#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <signal.h>
 
-int main()
+#include <iostream>
+#include <string>
+#include <list>
+#include <sys/stat.h>
+
+using namespace std;
+
+int main(int argc, char **argv)
 {
-  std::cerr << "client is not implemented yet" << std::endl;
+  // Check if received three command arguments
+  if (argc != 4)
+  {
+    cerr << "ERROR: Usage: " << argv[0] << " <HOSTNAME-OR-IP> <PORT> <FILENAME>" << endl;
+    exit(1);
+  }
+
+  // Set the hints for getaddrinfo()
+  struct addrinfo hints;
+  struct addrinfo *result;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_socktype = SOCK_DGRAM; // UDP socket
+  hints.ai_family = AF_INET;      // IPv4
+
+  // Get server address info using hints
+  // - argv[1]: HOSTNAME-OR-IP
+  // - argv[2]: PORT#
+  int ret;
+  if ((ret = getaddrinfo(argv[1], argv[2], &hints, &result)) != 0)
+  {
+    cerr << "ERROR: " << ret << endl;
+    exit(1);
+  }
+
+  sockaddr *serverSockAddr = result->ai_addr;
+  socklen_t serverSockAddrLength = result->ai_addrlen;
+
+  // Create a UDP socket
+  // - int socket(int domain, int type, int protocol)
+  int serverSockFd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  // Open file to transfer from client to server
+  // - argv[3]: FILENAME
+  int fileToTransferFd = open(argv[3], O_RDONLY);
+  if (fileToTransferFd == -1)
+  {
+    cerr << "ERROR: open()" << endl;
+    exit(1);
+  }
+
+  struct stat fdStat;
+  fstat(fileToTransferFd, &fdStat);
+  uint8_t fileBuffer[fdStat.st_size];
+  size_t bytesRead = read(fileToTransferFd, fileBuffer, fdStat.st_size);
+  cout << bytesRead << " bytes read" << endl;
+
+  sendto(serverSockFd, fileBuffer, bytesRead, MSG_CONFIRM, serverSockAddr, serverSockAddrLength);
+
+  cout << "DATA sent" << endl;
+
+  struct sockaddr addr;
+  socklen_t addr_len = sizeof(struct sockaddr);
+  memset(fileBuffer, 0, sizeof(fileBuffer));
+  ssize_t length = recvfrom(serverSockFd, fileBuffer, fdStat.st_size, 0, &addr, &addr_len);
+  string str((char *)fileBuffer);
+  cerr << "ACK reveived " << length << " bytes: " << endl
+       << str << endl;
+
+  close(fileToTransferFd);
+  exit(0);
 }
--- server.cpp
+++ server.cpp
@@ -1,8 +1,66 @@
 #include <string>
-#include <thread>
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+#include <signal.h>
+#include <inttypes.h>
+#include <unordered_map>
+#include <cstring>
 
-int main()
+using namespace std;
+
+int main(int argc, char **argv)
 {
-  std::cerr << "server is not implemented yet" << std::endl;
+
+  // check arguments
+  if (argc != 2)
+  {
+    cerr << "ERROR: Usage: " << argv[0] << " <PORT> " << endl;
+    exit(1);
+  }
+
+  // UDP socket
+  int serverSockFd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  struct addrinfo hints;
+  memset(&hints, '\0', sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_DGRAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  struct addrinfo *myAddrInfo;
+  int ret;
+  if ((ret = getaddrinfo(NULL, argv[1], &hints, &myAddrInfo)) != 0)
+  {
+    cerr << "error" << endl;
+    exit(1);
+  }
+
+  if (bind(serverSockFd, myAddrInfo->ai_addr, myAddrInfo->ai_addrlen) == -1)
+  {
+    cerr << "ERROR: bind()" << endl;
+    exit(1);
+  }
+
+  while (1)
+  {
+    char buf[1024]; // extra space to be safe
+    struct sockaddr addr;
+    socklen_t addr_len = sizeof(struct sockaddr);
+
+    ssize_t length = recvfrom(serverSockFd, buf, 1024, 0, &addr, &addr_len);
+    // string str(buf);
+    cerr << "DATA received " << length << " bytes from : " << inet_ntoa(((struct sockaddr_in *)&addr)->sin_addr) << endl;
+
+    length = sendto(serverSockFd, "ACK", strlen("ACK"), MSG_CONFIRM, &addr, addr_len);
+    cout << length << " bytes ACK sent" << endl;
+  }
+
+  exit(0);
 }
