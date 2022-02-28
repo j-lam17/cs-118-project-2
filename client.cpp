2ef85107b58f939f356469ff105fbdd7ce2a990c
Jonathan Lam <jlam7@g.ucla.edu>
2022-02-27 18:51:19 -0800

    Changed protocol for sending and receiving packets. Now utilizes a designated structure instead of a char array for the bytes to be sent. It can send and receive packets correctly, doing minimal parsing of the received packet. The 3-way handshake hasn't been completed on the server side, but that is the next step. It also now prints some of the required print statements when receiving and sending packets.

--- client.cpp
+++ client.cpp
@@ -1,8 +1,187 @@
 #include <string>
 #include <thread>
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <time.h>
+#include <signal.h>
+#include <errno.h>
+#include <netdb.h>
+#include <cstring>
+#include <vector>
+#include <fstream>
+using namespace std;
 
-int main()
+#define PACKET_SIZE 524
+
+typedef struct packet_t {
+  unsigned int sequence;
+  unsigned int acknowledgment;
+  unsigned short connectionID;
+  char empty;
+  char flags;
+  char payload[512]; 
+} packet_t;
+
+typedef struct conn_t {
+  // keeps track of where to send the packet back to 
+  struct sockaddr addr;
+  // keeping track of packets
+  short ID;
+  // initialize current sequence number to 4321
+  int currentSeq;
+  int currentAck; 
+  // need to add necessary congestion variables
+  int cwnd;
+  int ssthresh;
+  // ofstream to write to
+  ofstream *fs;
+} conn_t;
+
+
+void setA(packet_t &packet, bool b);
+void setS(packet_t &packet, bool b);
+void setF(packet_t &packet, bool b);
+bool getA(packet_t &packet);
+bool getS(packet_t &packet);
+bool getF(packet_t &packet);
+unsigned int payloadSize(packet_t &packet);
+void printPacketServer(packet_t &packet, conn_t *connection, bool recv);
+
+int main(int argc, char *argv[])
 {
-  std::cerr << "client is not implemented yet" << std::endl;
+  if (argc != 4)
+  {
+    cerr << "ERROR: Usage: " << argv[0] << " <HOSTNAME-OR-IP> <PORT> <FILENAME>" << endl;
+    exit(1);
+  }
+
+  // Set the hints for getaddrinfo()
+  struct addrinfo hints;
+  struct addrinfo *result;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_socktype = SOCK_DGRAM; // UDP socket
+  hints.ai_family = AF_INET;      // IPv4
+
+  // Get server address info using hints
+  // - argv[1]: HOSTNAME-OR-IP
+  // - argv[2]: PORT#
+  int ret;
+  if ((ret = getaddrinfo(argv[1], argv[2], &hints, &result)) != 0)
+  {
+    cerr << "ERROR: " << ret << endl;
+    exit(1);
+  }
+
+  sockaddr *serverSockAddr = result->ai_addr;
+  socklen_t serverSockAddrLength = result->ai_addrlen;
+
+  // Create a UDP socket
+  // - int socket(int domain, int type, int protocol)
+  int serverSockFd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  packet_t packet;
+  packet.sequence = 12345;
+  packet.acknowledgment = 0;
+  packet.connectionID = 0;
+  setA(packet, false);
+  setS(packet, true);
+  setF(packet, false);
+
+  strcpy(packet.payload, "Hello World\n");
+  cerr << "Payload: " << packet.payload;
+
+  // create a make shift connection
+  conn_t client_conn;
+  client_conn.ID = 0;
+  client_conn.cwnd = 512;
+
+  // print out packet sending
+  printPacketServer(packet, &client_conn, false);
+
+  // sending packet
+  sendto(serverSockFd, &packet, sizeof(packet_t), 0, serverSockAddr, serverSockAddrLength);
+  packet = {0};
+  int n = recvfrom(serverSockFd, &packet, sizeof(packet_t), 0, serverSockAddr, &serverSockAddrLength);
+  cerr << "Received: " << n << endl;
+
+  // print out packet received
+  client_conn.ID = packet.connectionID;
+  printPacketServer(packet, &client_conn, true);
+}
+
+void setA(packet_t &packet, bool b) {
+  char tmp = packet.flags;
+  // zero out previous flag
+  // 00000011
+  tmp &= 0x03;
+  // set A flag with value b
+  packet.flags = tmp | (b << 2); 
+}
+void setS(packet_t &packet, bool b) {
+  char tmp = packet.flags;
+  // zero out previous flag
+  // 00000101
+  tmp &= 0x05;
+  // set S flag with value b
+  packet.flags = tmp | (b << 1); 
+}
+void setF(packet_t &packet, bool b) {
+  char tmp = packet.flags;
+  // zero out previous flag
+  // 00000110
+  tmp &= 0x06;
+  // set A flag with value b
+  packet.flags = tmp | (b); 
+}
+
+bool getA(packet_t &packet) {
+  char tmp = packet.flags;
+  return (tmp & 0x04);
+}
+
+bool getS(packet_t &packet) {
+  char tmp = packet.flags;
+  return (tmp & 0x02);
+}
+
+bool getF(packet_t &packet) {
+  char tmp = packet.flags;
+  return (tmp & 0x01);
+}
+
+unsigned int payloadSize(packet_t &packet) {
+  return strlen(packet.payload);
+}
+
+// printing out packets
+// need to double check formatting
+void printPacketServer(packet_t &packet, conn_t *connection, bool recv) {
+  if (recv) {
+    cout << "RECV ";
+  }
+  else {
+    cout << "SEND ";
+  }
+  cout << packet.sequence << " " << packet.acknowledgment
+  << " " << connection->ID << " " << connection->cwnd;
+  
+  if (getA(packet)) {
+    cout << " ACK";
+  }
+
+  if (getS(packet)) {
+    cout << " SYN";
+  }
+
+  if (getF(packet)) {
+    cout << " FIN";
+  }
+
+  // add duplicate check at some point as well
+
+  // end of print statement, append newline
+  cout << endl;
 }
--- server.cpp
+++ server.cpp
@@ -8,7 +8,10 @@
 #include <time.h>
 #include <signal.h>
 #include <errno.h>
+#include <netdb.h>
 #include <cstring>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
@@ -19,22 +22,54 @@ using namespace std;
 #define RWND 51200
 #define INIT_SSTHRESH 10000
 #define INIT_SEQ 4321
-#define INIT_ACK 0
+#define PACKET_SIZE 524
 
 // user defined structs
-typedef struct header_t {
-  unsigned int seqNum;
-  unsigned int ackNum;
-  unsigned short connID;
-  bool A;
-  bool S;
-  bool F;
-} header_t;
+typedef struct packet_t {
+  unsigned int sequence;
+  unsigned int acknowledgment;
+  unsigned short connectionID;
+  char empty;
+  char flags;
+  char payload[512]; 
+} packet_t;
 
-void socket_setup(int port, int &server_fd, struct sockaddr_in &server, int len);
-void assemble_header(header_t *h, char *header);
-void parse_header(header_t *h, char *buf);
-void ThreeWayHandshake();
+typedef struct conn_t {
+  // keeps track of where to send the packet back to 
+  struct sockaddr addr;
+  // keeping track of packets
+  short ID;
+  // initialize current sequence number to 4321
+  int currentSeq = INIT_SEQ;
+  int currentAck; 
+  // need to add necessary congestion variables
+  int cwnd = MIN_CWND;
+  int ssthresh = INIT_SSTHRESH;
+  // ofstream to write to
+  ofstream *fs;
+} conn_t;
+
+bool synPacket(packet_t &incomingPacket);
+void ThreeWayHandshake(packet_t &incomingPacket, struct sockaddr &client);
+void connToHeader(conn_t *pConn, packet_t &packet);
+void setA(packet_t &packet, bool b);
+void setS(packet_t &packet, bool b);
+void setF(packet_t &packet, bool b);
+bool getA(packet_t &packet);
+bool getS(packet_t &packet);
+bool getF(packet_t &packet);
+unsigned int payloadSize(packet_t &packet);
+void printPacketServer(packet_t &packet, conn_t *connection, bool recv);
+
+// global variables
+// vector to keep track of outstanding connections
+vector <conn_t*> connections;
+// file directory to save the files to
+string file_directory;
+unsigned int currentConn = 1;
+int server_fd;
+socklen_t addr_len = sizeof(struct sockaddr);
+
 // client initiates with SYN, so need to wait to receive a SYN packet
 // before sending back SYN/ACK (no payload)
 // then, receive ACK from client, and start reading data
@@ -44,20 +79,28 @@ void ThreeWayHandshake();
 // single client
 // 2) window tracking function
 // keeping track of acks, sequences, cwnd, all of the stuff in TCP
-// 
+
 int main(int argc, char *argv[])
 {
+  // incorrect number of arguments provided
+  if (argc != 3) {
+    cerr << "ERROR: Usage: " << argv[0] <<  " <PORT> <FILE-DIR>" << endl;
+    exit(1);
+  }
+
   int port = atoi(argv[1]);
+  file_directory = argv[2];
 
-  int server_fd;
-  struct sockaddr_in server;
-  int len = sizeof(struct sockaddr_in);
-  socket_setup(port, server_fd, server, len);
-}
+  // struct to store metadata about location of incoming packet
+  struct sockaddr client;
+  // destination where data is received
+  packet_t incomingPacket;
+  // # of bytes received
+  int recvNum;
+  // indicates whether it is the start of a connection
+  bool syn;
 
-// always assume the folder is correct
-void socket_setup(int port, int &server_fd, struct sockaddr_in &server,
-  int len) {
+  // setting up server socket
   // check valid port number
   // within valid range
   // check negative values
@@ -70,92 +113,196 @@ void socket_setup(int port, int &server_fd, struct sockaddr_in &server,
     exit(1);
   }
 
-  if ((server_fd = socket(PF_INET, SOCK_DGRAM, 0)) == -1) {
+  if ((server_fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
     perror("server socket");
     exit(1);
   }
   
-  // INADDR_ANY allows server to accept connections from any interface
-  server.sin_addr.s_addr = htonl(INADDR_ANY);
-  server.sin_family = AF_INET;
-  server.sin_port = htons(port); // desired port is given from input variable
-  memset(&server.sin_zero, '\0', sizeof(server.sin_zero));
+  struct addrinfo hints;
+  memset(&hints, '\0', sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_DGRAM;
+  hints.ai_flags = AI_PASSIVE;
 
-  // binding server to socket
-  if (bind(server_fd, (struct sockaddr *) &server, sizeof(server)) == -1) {
-    perror("server socket bind");
+  struct addrinfo *myAddrInfo;
+  int ret;
+  if ((ret = getaddrinfo(NULL, argv[1], &hints, &myAddrInfo)) != 0)
+  {
+    cerr << "error" << endl;
     exit(1);
   }
 
+  if (bind(server_fd, myAddrInfo->ai_addr, myAddrInfo->ai_addrlen) == -1)
+  {
+    cerr << "ERROR: bind()" << endl;
+    exit(1);
+  }
+
+  while (true) {
+    // reset contents of incoming packet to be emtpy
+    incomingPacket = {0};
+
+    // recvNum = size of datagram read in
+    cerr << "Waiting\n";
+    recvNum = recvfrom(server_fd, &incomingPacket, PACKET_SIZE, 0, &client, &addr_len); 
+    cerr << "Received: " << recvNum << endl;
+    
+    // function that checks if it's a packet w SYN flag, ACK/FIN = 0
+    syn = synPacket(incomingPacket);
+
+    // conditional branch where the packet indicates a new connection
+    if (syn) {
+      // utilize 3 way handshake here
+      ThreeWayHandshake(incomingPacket, client);
+    }
+    // conditonal branch for a packet with an existing connection
+    else {
+      cerr << "Didn't enter\n";
+    }
+  }
 }
 
-void assemble_header(header_t *h, char *header) {
-    // clear any previous header data
-    // always 12 bytes of data
-    memset(header, 0, 12);
+// checks the status of the SYN flag bit in header
+bool synPacket(packet_t &incomingPacket) {
+  char t = incomingPacket.flags;
+  // want !A S & !F
+  return !((t >> 2) & 0x1) && ((t >> 1) & 0x1) && !(t & 0x1);
+}
 
-    // formatting sequence #
-    unsigned int seqNum = h->seqNum;
-    header[0] = (seqNum >> 24) & 0xFF;
-    header[1] = (seqNum >> 16) & 0xFF;
-    header[2] = (seqNum >> 8) & 0xFF;
-    header[3] = (seqNum) & 0xFF;
+// already know that the current buffer contains a SYN packet
+// create a new connection
+#include <iomanip>
+void ThreeWayHandshake(packet_t &incomingPacket, struct sockaddr &client) {
+  cerr << "Entered 3way\n";
+  conn_t *newC = new conn_t;
 
-    // formatting ack #
-    unsigned int ackNum = h->ackNum;
-    header[4] = (ackNum >> 24) & 0xFF;
-    header[5] = (ackNum >> 16) & 0xFF;
-    header[6] = (ackNum >> 8) & 0xFF;
-    header[7] = (ackNum) & 0xFF;
-
-    // formatting the connection ID
-    unsigned short connID = h->connID;
-    header[8] = (connID >> 8) & 0xFF;
-    header[9] = (connID) & 0xFF;
-
-    // formatting flag bits
-    // byte 10 was already zerod out from initialzation
-    char flag = 0;
-    bool A = h->A;
-    bool S = h->S;
-    bool F = h->F;
-    flag |= (A << 2);
-    flag |= (S << 1);
-    flag |= (F);
-    header[11] = flag;
-
-    // No need to return anything, since update happens in place
+  // indicates new connection
+  if (incomingPacket.connectionID == 0) {
+    newC->ID = currentConn;
+    // now expecting the next byte
+    newC->currentAck = incomingPacket.sequence + 1;
+    // deep copy of where to send response stored in client
+    newC->addr = client;
+    currentConn++;
+  } 
+  // syn packet for an already existing connection
+  else {
+    // delete newly allocated connection, since it exists
+    // potentially send an ACK to them
+    delete newC;
+    return;
   }
 
-  void parse_header(header_t *h, char *buf) {
-    // assuming that the first 12 bytes of received message contain header
-    unsigned int tmp = 0;
-    // store seqNum
-    tmp |= (buf[0] << 24);
-    tmp |= (buf[1] << 16);
-    tmp |= (buf[2] << 8);
-    tmp |= (buf[3]);
-    h->seqNum = tmp;
+  // storing payload from client
+  cerr << "Creating file\n";
+  // creating a new file at the corresponding directory
+  string fileNum = to_string(newC->ID);
+  string path = file_directory + "/" + fileNum + ".file";
 
-    // store ackNum
-    tmp = 0;
-    tmp |= (buf[4] << 24);
-    tmp |= (buf[5] << 16);
-    tmp |= (buf[6] << 8);
-    tmp |= (buf[7]);
-    h->ackNum = tmp;
+  ofstream *myF = new ofstream(path);
+  newC->fs = myF;
+  *myF << incomingPacket.payload;
+  // can only read from the file after closing
+  myF->close();
 
-    // store connID
-    tmp = 0;
-    tmp |= (buf[8] << 8);
-    tmp |= (buf[9]);
-    h->connID = tmp;
+  // printing out recevied packet
+  printPacketServer(incomingPacket, newC, true);
 
-    // storing flags
-    tmp = buf[11];
-    h->A = tmp & 0x04;
-    h->S = tmp & 0x02;
-    h->F = tmp & 0x01;
-    
-    // No need to return anything, since update happens in place
+  // can now reuse buffer since all information has been extracted
+  // clearing previous information for packet
+  incomingPacket = {0};
+
+  // sending response SYN-ACK
+  connToHeader(newC, incomingPacket);
+  setA(incomingPacket, true);
+  setS(incomingPacket, true);
+  setF(incomingPacket, false);
+
+  sendto(server_fd, &incomingPacket, PACKET_SIZE, 0, &newC->addr, addr_len);
+
+  // printing out sent packet
+  printPacketServer(incomingPacket, newC, false);
+
+  // add new connection 
+  connections.push_back(newC);
+}
+// converts a connection struct to corresponding header in packet
+void connToHeader(conn_t *pConn, packet_t &packet) {
+  packet.sequence = pConn->currentSeq;
+  packet.acknowledgment = pConn->currentAck;
+  packet.connectionID = pConn->ID;
+}
+
+void setA(packet_t &packet, bool b) {
+  char tmp = packet.flags;
+  // zero out previous flag
+  // 00000011
+  tmp &= 0x03;
+  // set A flag with value b
+  packet.flags = tmp | (b << 2); 
+}
+void setS(packet_t &packet, bool b) {
+  char tmp = packet.flags;
+  // zero out previous flag
+  // 00000101
+  tmp &= 0x05;
+  // set S flag with value b
+  packet.flags = tmp | (b << 1); 
+}
+void setF(packet_t &packet, bool b) {
+  char tmp = packet.flags;
+  // zero out previous flag
+  // 00000110
+  tmp &= 0x06;
+  // set A flag with value b
+  packet.flags = tmp | (b); 
+}
+
+bool getA(packet_t &packet) {
+  char tmp = packet.flags;
+  return (tmp & 0x04);
+}
+
+bool getS(packet_t &packet) {
+  char tmp = packet.flags;
+  return (tmp & 0x02);
+}
+
+bool getF(packet_t &packet) {
+  char tmp = packet.flags;
+  return (tmp & 0x01);
+}
+
+unsigned int payloadSize(packet_t &packet) {
+  return strlen(packet.payload);
+}
+
+// printing out packets
+// need to double check formatting
+void printPacketServer(packet_t &packet, conn_t *connection, bool recv) {
+  if (recv) {
+    cout << "RECV ";
   }
+  else {
+    cout << "SEND ";
+  }
+  cout << packet.sequence << " " << packet.acknowledgment
+  << " " << connection->ID << " " << connection->cwnd;
+  
+  if (getA(packet)) {
+    cout << " ACK";
+  }
+
+  if (getS(packet)) {
+    cout << " SYN";
+  }
+
+  if (getF(packet)) {
+    cout << " FIN";
+  }
+
+  // add duplicate check at some point as well
+
+  // end of print statement, append newline
+  cout << endl;
+}
